8d382b6a8af56d8c69ddd7a01c66ba484eac9cf2
Developer <dev@example.com>
2026-03-17 15:39:54 +0800

    chore: remove push.sh script (no longer needed with SSH)

--- push.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Push script for chenjie222/engine fork
-# Usage: ./push.sh
-
-echo "Pushing to fork repository..."
-export GIT_CONFIG_NOSYSTEM=1
-git push origin master "$@"
-echo "Done!"
